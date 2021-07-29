aff84e3c43285113fe82cf85cd65d46e9f614c68
fehmi-enes-eren <76635233+fehmi-enes-eren@users.noreply.github.com>
2021-07-28 19:21:21 -0700

    Footer is updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,19 +7,7 @@ export default function Footer() {
             <div className="footerTransition">
                 <h2 style={{textAlign:"center", padding:"50px"}}>Thank you for being with us <i class="fas fa-sun"></i></h2>
             </div>
-            <div>
-
-            </div>
-            <div id="footerDiv">
-                <ul style={{listStyleType:"none"}}>
-                    <li>
-                        About
-                    </li>
-                    <li>
-                        Contact Us
-                    </li>
-                </ul>
-            </div>
+            
             
         </>
     )
