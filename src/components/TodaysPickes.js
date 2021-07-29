a97937c27f3e5f302c04333d4669437169f1d2e7
fehmi-enes-eren <76635233+fehmi-enes-eren@users.noreply.github.com>
2021-07-29 13:08:34 -0700

    Todays Pickes update

--- src/components/TodaysPickes.js
+++ src/components/TodaysPickes.js
@@ -30,7 +30,7 @@ export default function TodaysPickes() {
     }
     useEffect(() => {
         if(state.length !== 0){
-            setTodaysRecipes([state[15], state[18], state[26], state[38]])
+            setTodaysRecipes([state[16], state[19], state[26], state[38]])
         }
     }, [loading])
 
